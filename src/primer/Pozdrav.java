f27d128d68c3a048220b48aa25d62261f4b7bdcf
Korisnik <Korisnik@Aleksa>
2018-01-11 16:58:06 +0100

    Sta bule kae

--- src/primer/Pozdrav.java
+++ src/primer/Pozdrav.java
@@ -2,6 +2,6 @@ package primer;
 
 public class Pozdrav {
 	public static void main(String[] args) {
-		System.out.println("Maksa Ledjenda");
+		System.out.println("Bule kae da Makso nije ledjenda...");
 	}
 }
