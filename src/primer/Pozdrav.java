fdf9fd94a8099c8775264f74d3231c6cb0b0d001
Aleksa Novkovic <dmi.aleksa.novkovic@student.pmf.uns.ac.rs>
2018-01-11 17:11:53 +0100

    merge branch 'nova_grana' and 'master'

--- src/primer/Pozdrav.java
+++ src/primer/Pozdrav.java
@@ -2,10 +2,7 @@ package primer;
 
 public class Pozdrav {
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("Maksa je ledjenda");
-=======
 		System.out.println("Bule kae da Makso nije ledjenda!!!");
->>>>>>> refs/heads/nova_grana
 	}
 }
