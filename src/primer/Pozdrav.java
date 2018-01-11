fd072cde2fb74434e2a99f68f5d8dbc22020e04e
Aleksa Novkovic <dmi.aleksa.novkovic@student.pmf.uns.ac.rs>
2018-01-11 17:23:47 +0100

    sta je maksa

--- src/primer/Pozdrav.java
+++ src/primer/Pozdrav.java
@@ -3,6 +3,5 @@ package primer;
 public class Pozdrav {
 	public static void main(String[] args) {
 		System.out.println("Maksa je ledjenda");
-		System.out.println("Bule kae da Makso nije ledjenda!!!");
 	}
 }
