d26dc079629cb5c639538fbef88cb23c12f051c1
Aleksa Novkovic <dmi.aleksa.novkovic@student.pmf.uns.ac.rs>
2018-01-11 17:07:58 +0100

    maksa ledjenda

--- src/primer/Pozdrav.java
+++ src/primer/Pozdrav.java
@@ -2,6 +2,6 @@ package primer;
 
 public class Pozdrav {
 	public static void main(String[] args) {
-		System.out.println("Bule kae da Makso nije ledjenda...");
+		System.out.println("Maksa je ledjenda");
 	}
 }
