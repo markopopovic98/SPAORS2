0f163e05080b2485b961d823eceb242ab0f8aeeb
markopopovic98 <markopoppriboj@gmail.com>
2018-01-19 14:09:22 +0100

    Izmenjen prvi ispis

--- src/primer/Pozdrav.java
+++ src/primer/Pozdrav.java
@@ -3,7 +3,7 @@ package primer;
 public class Pozdrav {
 	public static void main(String[] args) {
 
-                //koja je budala ovo pisala 
+                 //on je doktor pop i slusa pank rok 
 		System.out.println("Maksa je ledjenda");
 		System.out.println("I bule kaze isto");
 	}
