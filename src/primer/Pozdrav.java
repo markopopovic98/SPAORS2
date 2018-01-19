b4451bec00cfbdce2623222c19a454808966fe04
markopopovic98 <markopoppriboj@gmail.com>
2018-01-19 14:07:32 +0100

    ko pise ovo

--- src/primer/Pozdrav.java
+++ src/primer/Pozdrav.java
@@ -2,6 +2,8 @@ package primer;
 
 public class Pozdrav {
 	public static void main(String[] args) {
+
+                //koja je budala ovo pisala 
 		System.out.println("Maksa je ledjenda");
 		System.out.println("I bule kaze isto");
 	}
