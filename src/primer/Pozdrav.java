2ee6fe4a457584374a3ce98627d3f8b9baa5d323
markopopovic98 <markopoppriboj@gmail.com>
2018-01-19 14:10:58 +0100

    Istina istina

--- src/primer/Pozdrav.java
+++ src/primer/Pozdrav.java
@@ -3,7 +3,8 @@ package primer;
 public class Pozdrav {
 	public static void main(String[] args) {
 
-                 //on je doktor pop i slusa pank rok 
+
+                 //Istina jeste 
 		System.out.println("Maksa je ledjenda");
 		System.out.println("I bule kaze isto");
 	}
